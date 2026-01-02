7e0ec67e29517619929b0b321951ad9156948fdf
syedaquibsiddiq-ifelse <syedaquibsiddiq@ifelsetech.com>
2026-01-02 12:06:52 +0530

    build config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    host: true,
+    port: 4173,
+  },
   plugins: [
     react(),
     svgr({
